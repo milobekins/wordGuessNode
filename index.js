ee15966697774329ac2e620bf186f35a9a5a42e9
mbekins <milobekins@gmail.com>
2018-07-29 20:09:00 -0700

    touchup

--- index.js
+++ index.js
@@ -2,25 +2,64 @@ var Word = require("./word");
 var inquirer = require("inquirer");
 
 var count = 0;
-remainingGuesses = 10;
+var remainingGuesses = 10;
+var lettersGuessed = [];
+var match = false;
+var lettersCorrect1 = 0;
+var lettersCorrect2 = 0;
 
 var words = ["plank", "chocolate", "roar", "chase", "safari", "debate", "renew"];
-console.log("=============================")
-console.log("Welcome to word guess!")
-console.log("=============================")
-console.log(" ");
 
 var numberGuess = Math.floor(Math.random() * words.length);
 var wordGuess = words[numberGuess];
 
 var wordToGuess = new Word(wordGuess);
-wordToGuess.addLetters(wordToGuess.value);
-wordToGuess.wordDisplay();
 
-console.log(" ");
+var startGame = function() {
 
-var promptFunc = function(remainingGuesses) {
+    count = 0;
+    remainingGuesses = 10;
+    lettersGuessed = [];
+    match = false;
+    lettersCorrect1 = 0;
+    lettersCorrect2 = 0;
 
+    console.log("=============================")
+    console.log("Welcome to word guess!")
+    console.log("=============================")
+    console.log(" ");
+
+    numberGuess = Math.floor(Math.random() * words.length);
+    wordGuess = words[numberGuess];
+
+    wordToGuess = new Word(wordGuess);
+    wordToGuess.addLetters(wordToGuess.value);
+    wordToGuess.wordDisplay();
+
+    console.log(" ");
+    promptFunc(remainingGuesses, lettersCorrect2);
+}
+
+var playAgain = function() {
+    inquirer.prompt([
+        {
+            name: "playAgain",
+            type: "confirm",
+            message: "Do you want to play again?",
+        }
+    ]).then(function(answer) {
+        if (answer.playAgain) {
+            startGame();
+        }
+        else {
+            return;
+        }
+    })
+}
+
+
+var promptFunc = function(remainingGuesses, lettersCorrect2) {
+    lettersCorrect1 = lettersCorrect2;
     if (remainingGuesses > 0) {
         console.log("Guesses Remaining: " + remainingGuesses);
         inquirer.prompt([
@@ -29,31 +68,66 @@ var promptFunc = function(remainingGuesses) {
                 message: "Please Guess A Letter.."
             }
         ]).then(function(answer) {
-            remainingGuesses--; 
-            wordToGuess.guess(answer.letterInput);
-            count = 0;
-            for (i=0; i < wordToGuess.letters.length; i++) {
-                if (wordToGuess.letters[i].guessed === true) {
-                    count++
+            match = false;
+            for (i=0; i < lettersGuessed.length; i++) {
+                if (answer.letterInput === lettersGuessed[i]) {
+                    match = true;
                 }
             }
             
-            if (count === wordToGuess.letters.length) {
-                wordToGuess.wordDisplay();
-                console.log("You Win");
-                return
-            }
-            else if (remainingGuesses === 0) {
-                wordToGuess.wordDisplay();
-                console.log("You Lost");
-                return
+            if (!match) {
+                lettersGuessed.push(answer.letterInput);            
+                wordToGuess.guess(answer.letterInput);
+                count = 0;
+                
+                for (i=0; i < wordToGuess.letters.length; i++) {
+                    if (wordToGuess.letters[i].guessed === true) {
+                        count++;
+                    }
+                }
+
+                lettersCorrect2 = count;
+
+                
+                if (count === wordToGuess.letters.length) {
+                    wordToGuess.wordDisplay();
+                    console.log("You Win");
+                    playAgain();
+                }
+                else {
+
+                    if (lettersCorrect2 > lettersCorrect1) {
+                        console.log("Correct!");
+                        wordToGuess.wordDisplay();
+                        console.log(" ");
+                        promptFunc(remainingGuesses, lettersCorrect2);
+                    }
+                    else {
+                        remainingGuesses--;
+                        console.log("Incorrect.")
+
+                        if (remainingGuesses === 0) {
+                            console.log("You Lost :(")
+                            console.log("Correct Word: " + wordGuess);
+                            console.log(" ");
+                            playAgain();
+                        }
+                        else {
+                            wordToGuess.wordDisplay();
+                            console.log(" ");
+                            promptFunc(remainingGuesses, lettersCorrect2);
+                        }
+                    }     
+                }
             }
             else {
-                wordToGuess.wordDisplay();
-                promptFunc(remainingGuesses);
+                console.log("Letter Already Guessed");
+                console.log(" ");
+                promptFunc(remainingGuesses, lettersCorrect2);
             }
+            
             
         })
     }  
 }
-promptFunc(remainingGuesses);
+startGame();
